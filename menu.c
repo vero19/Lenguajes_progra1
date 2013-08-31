bc57cea1822ddc2e7a4e79588f428feb9dab8b7a
Veronica <veronikamvm@gmail.com>
2013-08-31 13:44:36 -0600

    version de menu

---
+++ menu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+
+	int opcion;
+	printf("---- Menu ---\n");
+	printf("1. Agregar amigos\n");
+	printf("2. Enviar mensaje\n");
+	printf("3.Salir\n");
+	printf("\n");
+	printf("Ingrese la opcion deseada: ");
+	scanf("%d",&opcion);
+}
